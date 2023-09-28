45487574fcfa62c993a904d73464c973e5dd9ef3
Triciapeyz <142312614+Triciapeyz@users.noreply.github.com>
2023-09-28 14:53:30 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+name='tricia'
+age = 45
+gender ='f'
+marks = 48.9
+
+print(name,)
+print(age)
+print(gender)
+print(marks)
+
+student = ['sarah',100,'f'46]
+
+def function_name(time1,time2):
+    print(time1)
+    print(time2)
+    return time1
